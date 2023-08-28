68e00e9420e2f6f75eb7126c35161722850d4ae7
namdh03 <duonghoangnam@gmail.com>
2023-08-28 10:17:56 +0700

    Code refactoring and optimization #1

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -3,7 +3,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // optional
 import { Link } from 'react-router-dom';
 
-import routesConfig from '~/config/routes';
+import config from '~/config';
 import styles from './Header.module.scss';
 import images from '~/assets/img';
 import Button from '~/components/Button';
@@ -22,7 +22,7 @@ import {
     UploadIcon,
 } from '~/components/Icons';
 import Image from '~/components/Image';
-import Search from '~/components/Layout/components/Search';
+import Search from '~/layouts/components/Search';
 
 const cx = classNames.bind(styles);
 
@@ -100,7 +100,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="TikTok" />
                 </Link>
 
